172d0f66936ad5ff039a75f35d9fadbf91ab981d
David Gessner <davidges@gmail.com>
2013-07-09 17:11:14 +0200

    added letter rules
    I imported them from
    https://github.com/danielgm/JarvisGrammars/blob/master/vim.py

--- gvim.py
+++ gvim.py
@@ -4,6 +4,8 @@
 # Licensed under the LGPL, see <http://www.gnu.org/licenses/>
 #
 # Modified by David Gessner
+# Contains some code obtained from
+# https://github.com/danielgm/JarvisGrammars/blob/master/vim.py
 
 """
 Command-module for cursor movement and **editing**
@@ -82,6 +84,134 @@ from dragonfly import *
 
 release = Key("shift:up, ctrl:up")
 
+class LetterRule(MappingRule):
+	exported = True
+	mapping = {
+		'alpha': Key('a', static=True),
+		'bravo': Key('b', static=True),
+		'charlie': Key('c', static=True),
+		'dixie': Key('d', static=True),
+		'echo': Key('e', static=True),
+		'foxtrot': Key('f', static=True),
+		'golf': Key('g', static=True),
+		'hotel': Key('h', static=True),
+		'india': Key('i', static=True),
+		'juliet': Key('j', static=True),
+		'kilo': Key('k', static=True),
+		'lima': Key('l', static=True),
+		'mike': Key('m', static=True),
+		'november': Key('n', static=True),
+		'oscar': Key('o', static=True),
+		'papa': Key('p', static=True),
+		'quebec': Key('q', static=True),
+		'romeo': Key('r', static=True),
+		'sierra': Key('s', static=True),
+		'tango': Key('t', static=True),
+		'uniform': Key('u', static=True),
+		'victor': Key('v', static=True),
+		'whiskey': Key('w', static=True),
+		'x-ray': Key('x', static=True),
+		'yankee': Key('y', static=True),
+		'zulu': Key('z', static=True),
+
+		'upper alpha': Key('A', static=True),
+		'upper bravo': Key('B', static=True),
+		'upper charlie': Key('C', static=True),
+		'upper dixie': Key('D', static=True),
+		'upper echo': Key('E', static=True),
+		'upper foxtrot': Key('F', static=True),
+		'upper golf': Key('G', static=True),
+		'upper hotel': Key('H', static=True),
+		'upper india': Key('I', static=True),
+		'upper juliet': Key('J', static=True),
+		'upper kilo': Key('K', static=True),
+		'upper lima': Key('L', static=True),
+		'upper mike': Key('M', static=True),
+		'upper november': Key('N', static=True),
+		'upper oscar': Key('O', static=True),
+		'upper papa': Key('P', static=True),
+		'upper quebec': Key('Q', static=True),
+		'upper romeo': Key('R', static=True),
+		'upper sierra': Key('S', static=True),
+		'upper tango': Key('T', static=True),
+		'upper uniform': Key('U', static=True),
+		'upper victor': Key('V', static=True),
+		'upper whiskey': Key('W', static=True),
+		'upper x-ray': Key('X', static=True),
+		'upper yankee': Key('Y', static=True),
+		'upper zulu': Key('Z', static=True),
+
+		'0': Key('0'),
+		'1': Key('1'),
+		'2': Key('2'),
+		'3': Key('3'),
+		'4': Key('4'),
+		'5': Key('5'),
+		'6': Key('6'),
+		'7': Key('7'),
+		'8': Key('8'),
+		'9': Key('9'),
+
+		'space': Key('space'),
+		'tab': Key('tab'),
+
+		'ampersand': Key('ampersand'),
+		'apostrophe': Key('apostrophe'),
+		'asterisk': Key('asterisk'),
+		'at': Key('at'),
+		'backslash': Key('backslash'),
+		'backtick': Key('backtick'),
+		'bar': Key('bar'),
+		'caret': Key('caret'),
+		'colon': Key('colon'),
+		'comma': Key('comma'),
+		'dollar': Key('dollar'),
+		'dot': Key('dot'),
+		'double quote': Key('dquote'),
+		'equal': Key('equal'),
+		'exclamation': Key('exclamation'),
+		'hash': Key('hash'),
+		'hyphen': Key('hyphen'),
+		'minus': Key('minus'),
+		'percent': Key('percent'),
+		'plus': Key('plus'),
+		'question': Key('question'),
+#'semicolon': Key('semicolon'), # Getting Invalid key name: 'semicolon'
+		'slash': Key('slash'),
+		'[single] quote': Key('squote'),
+		'tilde': Key('tilde'),
+		'underscore | score': Key('underscore'),
+
+		'angle left': Key('langle'),
+		'brace left': Key('lbrace'),
+		'bracket left': Key('lbracket'),
+		'paren left': Key('lparen'),
+		'angle right': Key('rangle'),
+		'brace right': Key('rbrace'),
+		'bracket right': Key('rbracket'),
+		'paren right': Key('rparen'),
+	}
+
+letter = RuleRef(rule=LetterRule(), name='letter')
+letter_sequence = Repetition(letter, min=1, max=32, name='letter_sequence')
+
+def executeLetter(letter):
+	letter.execute()
+
+def executeLetterSequence(letter_sequence):
+	for letter in letter_sequence:
+		letter.execute()
+
+def findOnce(letter, n):
+	Key('f').execute()
+	letter.execute()
+
+def find(letter, n):
+	f = Key('f')
+	for i in range(n):
+		f.execute()
+		letter.execute()
+
 
 #---------------------------------------------------------------------------
 # Set up this module's configuration.
@@ -157,8 +287,9 @@ config.cmd.map    = Item(
 	"append":                       Key("a"),
 	"shift append":                       Key("A"),
 
-	"[<n>] find":                            Key("f"),
-	"[<n>] shift find":                            Key("F"),
+	'[<n>] find <letter>': Text('%(n)df') + Function(executeLetter),
+	'[<n>] shift find <letter>': Text('%(n)dF') + Function(executeLetter),
+
 
 	"[<n>] until":                            Key("t"),
 	"[<n>] shift until":                            Key("t"),
@@ -247,6 +378,8 @@ class KeystrokeRule(MappingRule):
 
     mapping  = config.cmd.map
     extras   = [
+		letter,
+		letter_sequence,
                 IntegerRef("n", 1, 100),
                 Dictation("text"),
                 Dictation("text2"),
